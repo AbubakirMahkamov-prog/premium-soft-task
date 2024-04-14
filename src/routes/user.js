d79dda4fc4e6d6810b57cb10b2a1946dc3ccdca6
AbubakirMahkamov-prog <mahkamovabubakir65119@gmail.com>
2024-04-14 11:38:10 +0500

    permissions by role

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,11 +3,11 @@ import Router from "koa-router";
 import userSchema from "../middlewares/validators/userValidator.js";
 import UserController from "../controllers/userController.js";
 import auth from "../middlewares/auth.js";
-
+import { roles } from "../utils/roles.js";
 const userController = new UserController();
 const userRouter = new Router({ prefix: '/user' });
 
-userRouter.post("/", auth(), joiMiddleware(userSchema.create), userController.create)
+userRouter.post("/", auth(roles.Admin), joiMiddleware(userSchema.create), userController.create)
 userRouter.patch("/:id", joiMiddleware(userSchema.update), userController.update)
 userRouter.get("/", userController.getAll)
 userRouter.get("/:id", userController.getOne)
